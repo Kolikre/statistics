a978dadc7c32938dd86897f13f1ba01b094b8122
Kola <ryzhuknick@gmail.com>
2021-03-01 21:42:46 +0200

    added put and delete methods

--- backend/room/urls.py
+++ backend/room/urls.py
@@ -5,7 +5,9 @@ from room import views
 urlpatterns = [
     # path('room/<int:pk>', TeamView.as_view()),
     path('room/', views.teams),
+    path('room/<int:pk>', views.team),
     path('players/', views.players_list),
-    path('coaches/', views.coaches_list),
     path('player/<int:pk>', views.player_detail),
+    path('coaches/', views.coaches_list),
+    path('coach/<int:pk>', views.coach_detail),
 ]
--- backend/room/views.py
+++ backend/room/views.py
@@ -32,6 +32,32 @@ def teams(request):
             return Response(team_data.errors)
 
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def team(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        team = Team.objects.get(pk=pk)
+    except Team.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TeamSerializers(team)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = TeamPostSerializers(team, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        team.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 @api_view(['GET', 'POST'])
 def players_list(request):
     """
@@ -104,3 +130,29 @@ def coaches_list(request):
             return Response({"status": True})
         else:
             return Response(coach_data.errors)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def coach_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        coach = Coach.objects.get(pk=pk)
+    except Coach.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CoachSerializers(coach)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CoachPostSerializers(coach, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        coach.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
